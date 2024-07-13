ea925873a8c5c8f06f7f3dc38894c59402913a74
Ossama Jamal Eddin <120677010+OssamaJamalEddin@users.noreply.github.com>
2024-07-13 03:07:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import Encoding
+import Decoding
+
+
+running = 'YES'
+while running == 'YES':
+    decision = input("Would you like to decode or encode ? \nEnter: ").upper()
+    message = input("Enter the message: ")
+    shifting = int(input("Enter the shifting number: "))
+    output = ""
+    if decision == "ENCODE":
+        output = Encoding.encoding_function(message, shifting)
+    elif decision == "DECODE":
+        output = Decoding.decoding_function(message, shifting)
+    else:
+        # raise Exception("Type error occurred, make sure to write (Encode/Decode) correctly")
+        print("Make sure you wrote Encode/Decode correctly.")
+        break
+    print(output)
+    running = input("Want to encode/decode more ? (Yes / No): ").upper()
+# import dec_enc
+#
+#
+# running = "YES"
+# while running == "YES":
+#     decision = input("Would you like to decode or encode ? \nEnter: ").upper()
+#     message = input("Enter the message: ")
+#     shifting = int(input("Enter the shifting number: "))
+#     dec_enc.ceaser_cypher(decision, message, shifting)
+#     running = input("Want to encode/decode more ? (Yes / No): ").upper()
